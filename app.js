549354c49097d85b46c5d9b654560cbb04b9ed9f
hernangit config --global user.name John <hernan0066@gmail.com>
2021-12-12 13:56:31 -0300

    4.34

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 
 const { crearTabla } = require("./helpers/multiplicar");
+const argv = require("yargs").argv;
 
 console.clear();
 
-const [,, arg3 = 'base=3'] = process.argv;
-const [ , base = 5] = arg3.split('=');
+//const [,, arg3 = 'base=3'] = process.argv;
+//const [ , base = 5] = arg3.split('=');
 
 //const base = 5;
 /* crearTabla(base)
     .then(nombreArchivo => console.log(`Archivo creado: ${nombreArchivo}`))
     .catch(err => console.log(err)); */
+
